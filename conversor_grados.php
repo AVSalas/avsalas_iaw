4187d02c6cf35adb61f1f762dbebdfa519673290
AVSalas <albertodelvalle1992@gmail.com>
2020-01-16 18:16:45 +0100

    Actividades IAW - 16.1.20

---
+++ conversor_grados.php
@@ -0,0 +1,10 @@
+<?php
+
+//Conversor grados
+
+$celsius = 27.5;
+$kelvin = 273 + $celsius;
+$fahrenheit = 1.8 * $celsius + 32;
+
+echo $celsius." grados Celsius son ".$fahrenheit." grados Fahrenheit y ".$kelvin." grados Kelvin.";
+?>
